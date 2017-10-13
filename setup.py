17c60665851136ad74ad1e50915615d7b89f76aa
dgursoy <sparsedata@gmail.com>
2017-10-13 13:00:39 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='adonis',
+    author='Barbara Frosnik, Doga Gursoy',
+    packages=find_packages(),
+    version=open('VERSION').read().strip(),
+    description = 'Unit-testing for scientific data.',
+    license='BSD-3',
+    platforms='Any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: BSD-3',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
