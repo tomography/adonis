b374159efa7d9b2c4427210b0efdd8606cd48159
Barbara Frosik <bfrosik@anl.gov>
2017-12-13 15:02:19 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
 
 setup(
     name='censor',
@@ -16,10 +16,10 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: BSD-3',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
+       # 'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+       # 'Programming Language :: Python :: 3',
+       # 'Programming Language :: Python :: 3.5',
+       # 'Programming Language :: Python :: 3.6',
+    ]
 )
